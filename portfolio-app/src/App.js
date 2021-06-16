6e028f51744f6aea05a89e07320cef2573a1cf49
JoshDaos <joshuadaos1998@gmail.com>
2021-06-16 00:37:15 -0700

    added header

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// imports
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-app/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <header class="header">
+            <div>
+                <h1>Josh Daos</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
