e23b11174d7040b577eff07bc77e905f9094b2ae
JoshDaos <joshuadaos1998@gmail.com>
2021-06-17 14:01:00 -0700

    horizontal look on nav

--- portfolio-app/src/components/Nav/nav.js
+++ portfolio-app/src/components/Nav/nav.js
@@ -4,12 +4,12 @@ function Nav() {
     return(
         <nav className="navbar">
             <ul className="nav__links">
-                <li><a href="">Joshua</a></li>
-                <li><a href="">Intro</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Contact</a></li>
+                <li className="nav__links-li"><a href="">Joshua</a></li>
+                <li className="nav__links-li"><a href="">Intro</a></li>
+                <li className="nav__links-li"><a href="">About</a></li>
+                <li className="nav__links-li"><a href="">Projects</a></li>
+                <li className="nav__links-li"><a href="">Blog</a></li>
+                <li className="nav__links-li"><a href="">Contact</a></li>
             </ul>
         </nav>
     )
