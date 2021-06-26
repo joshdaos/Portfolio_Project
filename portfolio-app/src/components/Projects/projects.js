cb47d298a3101b05ab76f2f969ddfb9661db396f
JoshDaos <joshuadaos1998@gmail.com>
2021-06-26 15:44:06 -0700

    css in project sec

--- portfolio-app/src/components/Projects/projects.js
+++ portfolio-app/src/components/Projects/projects.js
@@ -27,21 +27,23 @@ function Projects() {
 
         </div>
         <div className="container">
-        <img src={ProjectOne} alt="Avatar" className="image" width="100%"/>
+        <img src="https://media3.giphy.com/media/4LTGEdPueINFzycY1Ixq/200.webp?cid=ecf05e472q8rjtjl31sfaag11ljvv4f1x4gg1j01okhiowf9&rid=200.webp&ct=g" alt="Avatar" className="image" width="100%"/>
         <div className="middle">
             <div className="text">
                 <p>Grootagotchi</p>
                 <a href="">Live Site</a>
             </div>
         </div>
-        
+
         </div>
         <div className="container">
         <img src={ProjectOne} alt="Avatar" className="image" width="100%"/>
         <div className="middle">
             <div className="text">
-                <p>Grootagotchi</p>
-                <a href="">Live Site</a>
+                <h1>Grootagotchi</h1>
+                <p>HTML, CSS, JAVASCRIPT, and JQUERY</p>
+                <a href="">Live Site<br></br></a>
+                <a href="">GitHub</a>
             </div>
         </div>
         </div>
