cd537735e46dc0ce4b3a6ed3c898237cf990c89f
JoshDaos <joshuadaos1998@gmail.com>
2021-06-26 19:49:23 -0700

    css things

--- portfolio-app/src/components/About/about.js
+++ portfolio-app/src/components/About/about.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ProfilePic from '../../images/me.jpg';
 
 function About() {
     return(
         <div id='about' className="about__content">
             <h1>About</h1>
-            <p>Driven by my curiosity in tech & possessing a passion for solving complex problems, I am a full-stack engineer who enjoys transforming applications from logic to production.
-
-            With a strong background in delivering exceptional results in sales and providing solutions to customers, I am motivated to hone my problem-solving skills as a programmer. I am eager to maximize my recent General Assembly Software Engineering immersive education, where I became fluent in Programming Languages, along with Computational-Thinking, and CS Fundamentals. I believe I have a purpose to impact people by creating applications and platforms that house solutions or skills to societal issues. My overall goal is to be a part of a huge cause that delivers excellence to people.</p>
+            <img className="profile__image" src={ProfilePic} />
+            <p className="header__p">
+                Hello. My name is Josh Daos.<br></br> I am Full-Stack Developer with a passion for UI Design.
+            </p>
         </div>
     )
 }
