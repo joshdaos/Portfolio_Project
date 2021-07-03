6dae0fbac8a61d98d701d52df86fd27a9033c308
JoshDaos <joshuadaos1998@gmail.com>
2021-07-03 13:36:54 -0700

    added some project images

--- portfolio-app/src/components/Projects/projects.js
+++ portfolio-app/src/components/Projects/projects.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ProjectOne from '../../images/grootagotchi.jpg'
+import ProjectTwo from '../../images/gitsumcoffee.jpg'
+import ProjectThree from '../../images/wayfarer.jpg'
+import ProjectFour from '../../images/dripsy.jpg'
+
 
 function Projects() {
     return(
@@ -17,7 +21,7 @@ function Projects() {
 
         </div>
         <div className="container">
-        <img src={ProjectOne} alt="Avatar" className="image" width="100%"/>
+        <img src={ProjectTwo} alt="Avatar" className="image" width="100%"/>
         <div className="middle">
             <div className="text">
                 <p>Grootagotchi</p>
@@ -27,7 +31,7 @@ function Projects() {
 
         </div>
         <div className="container">
-        <img src="https://media3.giphy.com/media/4LTGEdPueINFzycY1Ixq/200.webp?cid=ecf05e472q8rjtjl31sfaag11ljvv4f1x4gg1j01okhiowf9&rid=200.webp&ct=g" alt="Avatar" className="image" width="100%"/>
+        <img src={ProjectThree} alt="Avatar" className="image" width="100%"/>
         <div className="middle">
             <div className="text">
                 <p>Grootagotchi</p>
@@ -37,7 +41,7 @@ function Projects() {
 
         </div>
         <div className="container">
-        <img src={ProjectOne} alt="Avatar" className="image" width="100%"/>
+        <img src={ProjectFour} alt="Avatar" className="image" width="100%"/>
         <div className="middle">
             <div className="text">
                 <h1>Grootagotchi</h1>
