fdbcca88e2aabc48eec172fdda5018425cd592e4
JoshDaos <joshuadaos1998@gmail.com>
2021-07-13 23:16:09 -0700

    added some skills and more content in about

--- portfolio-app/src/components/About/about.js
+++ portfolio-app/src/components/About/about.js
@@ -6,8 +6,13 @@ function About() {
         <div id='about' className="about__content">
             <h1>About</h1>
             <img className="profile__image" src={ProfilePic} />
+            <h5>Bay Area, CA</h5>
             <p className="header__p">
-                Hello. My name is Josh Daos.<br></br> I am Full-Stack Developer with a passion for UI Design.
+                Hello. My name is Josh Daos.<br></br> I am a software engineer located in Fairfield, CA. I previously spent three years in Retail learning the ins-and-outs of consumer support, but switched over to coding in an effort to find job stability and a better work life balance. I've worked at Macy's and The Home Depot, where I became fluent in customer support, customer care, client relations, and support service. My experience at these two diverse retail environments has upskilled my work ethic, my attention to detail, and my ability to problem solve.
+            </p>
+
+            <p className="header__p">
+                I always had a strong curiousity towards tech. Growing up I was...
             </p>
         </div>
     )
--- portfolio-app/src/components/Nav/nav.js
+++ portfolio-app/src/components/Nav/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Nav() {
     return(
-        <nav className="navbar">
+        <nav id="navbar" className="navbar">
             <ul className="nav__links">
                 <li id="home__link" className="nav__links-li"><a className="anchor"href="#home">JRD</a></li>
                 <li className="nav__links-li"><a className="anchor" href="#about">About</a></li>
